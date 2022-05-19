58dad1000f639bc58ade5963afc656329de872d7
tidwall <joshbaker77@gmail.com>
2022-05-19 09:00:47 -0700

    Added result formatter

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::{Instant, Duration};
+
 pub fn ops(nops: usize, threads: usize, mut f: impl FnMut(usize, usize)) {
     if threads != 1 {
         panic!("support for multiple threads is not supported yet");
@@ -9,7 +11,7 @@ pub fn ops(nops: usize, threads: usize, mut f: impl FnMut(usize, usize)) {
     };
     let mut i = 0;
     let mut remaining = nops;
-    let now = std::time::Instant::now();
+    let now = Instant::now();
     for _ in 0..threads {
         let take = if remaining > spread {
             spread
@@ -22,18 +24,7 @@ pub fn ops(nops: usize, threads: usize, mut f: impl FnMut(usize, usize)) {
             i += 1;
         }
     }
-    let elapsed = now.elapsed();
-    let nano = elapsed.as_nanos();
-    let ms = elapsed.as_millis();
-    let secs = nano as f64 / 1e9;
-    let ops_sec = ((nops as f64) / secs) as u64;
-    let nsop = (nano as f64 / nops as f64) as u64;
-
-    print!("{} ops", commaize(nops as u64));
-    if threads != 1 {
-        print!(" over {} threads", threads);
-    }
-    println!(" in {}ms, {}/sec, {} ns/op", ms, commaize(ops_sec), nsop);
+    println!("{}", res_string(nops, threads, now.elapsed()));
 }
 
 fn commaize(num: u64) -> String {
@@ -51,4 +42,20 @@ fn commaize(num: u64) -> String {
         j += 1;
     }
     s2
+}
+
+pub fn res_string(nops: usize, threads: usize, elapsed: Duration) -> String {
+    let nano = elapsed.as_nanos();
+    let ms = elapsed.as_millis();
+    let secs = nano as f64 / 1e9;
+    let ops_sec = ((nops as f64) / secs) as u64;
+    let nsop = (nano as f64 / nops as f64) as u64;
+
+    let mut out = String::new();
+    out += &format!("{} ops", commaize(nops as u64));
+    if threads != 1 {
+        out += &format!(" over {} threads", threads);
+    }
+    out += &format!(" in {}ms, {}/sec, {} ns/op", ms, commaize(ops_sec), nsop);
+    out
 }
